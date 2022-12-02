679c58f27258cb357fb3a1d79b52b80325bc1f4c
Martijn Dorsman <martijn.dorsman@gmail.com>
2022-12-02 15:07:04 +0100

    Add styling

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,18 @@ import { computed, ref } from 'vue';
 import { shuffle } from './utils';
 import { GameState, Team, Turn } from './types';
 
-const subjects = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(n => n.toString());
+const subjects = [
+  'Twilight saga',
+  'Finding Nemo',
+  'Robert ten Brink',
+  'Marco van Basten',
+  'Radio 538',
+  'Venco',
+  'JK Rowling',
+  'Sloffen',
+  'Cameron Diaz',
+  'De Fabeltjeskrant',
+  'The Joker'];
 const chosenSubjects = ref<string[]>([]);
 
 const maxScore = ref(15);
@@ -35,24 +46,33 @@ const teams = ref<Team[]>([
   { name: 'Team 2', player0: 'Player 3', player1: 'Player 4', score: 0 }
 ]);
 const winningTeam = computed(() => teams.value.find(team => team.score >= maxScore.value));
-
-function onSubmit() {
+const sortedTeams = computed(() => teams.value.sort((teamA, teamB) => teamB.score - teamA.score));
+const teamInputRef = ref<HTMLInputElement | null>(null);
+function onAddTeam() {
   teams.value = [...teams.value, { name: name.value, player0: input1.value, player1: input2.value, score: 0 }];
   name.value = '';
   input1.value = '';
   input2.value = '';
+  teamInputRef.value?.focus();
 }
 
 function onClickPlay() {
   gameState.value = GameState.PlayerReady;
 }
 
+const SECONDS = 30;
+
 function onClickReady() {
   chosenSubjects.value = shuffle(subjects).slice(0, 5);
   gameState.value = GameState.TurnActive;
+  remainingSeconds.value = SECONDS;
+  const interval = setInterval(() => {
+    remainingSeconds.value--;
+  }, 1000);
   setTimeout(() => {
-    gameState.value = GameState.GameOver
-  }, 1 * 1000)
+    gameState.value = GameState.GameOver;
+    clearInterval(interval);
+  }, SECONDS * 1000)
 }
 
 function onConfirmScore() {
@@ -80,18 +100,21 @@ function onPlayAgain() {
   turn.value = { teamIndex: 0, playerIndex: 0 };
   gameState.value = GameState.Setup;
 }
+const remainingSeconds = ref(0);
+const countDownWidth = computed(() => `${(remainingSeconds.value / SECONDS) * 100}%`)
 </script>
 
 <template>
-  <div class="text-white max-w-screen-md mx-auto p-4">
-    <h1 class="text-white text-center mb-4">30 Seconds</h1>
+  <div class="text-white max-w-screen-sm mx-auto p-4 font-base text-2xl">
+    <h1 class="text-center mb-4 font-semibold text-2xl">30 Seconds</h1>
 
     <template v-if="gameState === GameState.Setup">
-      <form @submit.prevent="onSubmit" class="flex flex-col space-y-2 text-gray-900">
-        <input class="px-4 py-2 rounded" type="text" placeholder="Team name" v-model="name">
-        <input class="px-4 py-2 rounded" type="text" placeholder="Player 1" v-model="input1">
-        <input class="px-4 py-2 rounded" type="text" placeholder="Player 2" v-model="input2">
-        <button class="bg-blue-400 rounded p-2 my-2 text-white">Add team</button>
+      <h2 class="font-semibold text-xl my-2">Create teams</h2>
+      <form @submit.prevent="onAddTeam" class="flex flex-col space-y-2 text-gray-900">
+        <input ref="teamInputRef" class="px-4 py-2 rounded" type="text" placeholder="Team name" v-model="name" required>
+        <input class="px-4 py-2 rounded" type="text" placeholder="Player 1" v-model="input1" required>
+        <input class="px-4 py-2 rounded" type="text" placeholder="Player 2" v-model="input2" required>
+        <button type="submit" class="bg-yellow-800 rounded p-2 my-2 text-white font-semibold">Add team</button>
       </form>
       <h2 class="font-semibold text-xl my-2">Teams</h2>
       <div v-for="team of teams">
@@ -100,47 +123,67 @@ function onPlayAgain() {
 
       <h2 class="font-semibold text-xl my-2">Max. score</h2>
       <form @submit.prevent="onClickPlay" class="text-gray-900 flex flex-col">
-        <input class="px-4 py-2 rounded" type="number" placeholder="Max. score" v-model="maxScore">
-        <button type="submit" class="bg-blue-400 rounded py-2 px-4 my-2 text-white">Play</button>
+        <input class="px-4 py-2 rounded mb-2" type="number" placeholder="Max. score" v-model="maxScore">
+        <button type="submit" class="bg-yellow-800 rounded py-2 px-4 my-2 text-white font-semibold">Play game</button>
       </form>
 
     </template>
 
     <template v-if="gameState === GameState.PlayerReady">
-      <h2>Give the phone to {{ currentPlayer }}</h2>
-
-      <h2>Scores</h2>
-
-      <ul>
-        <li v-for="team of teams">{{ team.name }}: {{ team.score }}</li>
-      </ul>
-
-      <button @click="onClickReady" class="bg-blue-400 rounded py-2 px-4 my-2 text-white">Ready</button>
+      <div class="text-center flex flex-col">
+        <h2 class="font-semibold text-5xl my-8">{{ currentPlayer }} <br> are you ready?</h2>
+        <h2 class="font-semibold text-2xl my-2">Scores</h2>
+        <div class="grid grid-cols-2 mb-6">
+          <template v-for="team of teams">
+            <div class="font-semibold">{{ team.name }}</div>
+            <div>{{ team.score }}</div>
+          </template>
+        </div>
+        <button @click="onClickReady"
+          class="bg-yellow-800 rounded py-2 px-4 my-2 text-white font-semibold">Ready</button>
+      </div>
     </template>
 
     <template v-if="(gameState === GameState.TurnActive)">
-      <ul>
-        <li v-for="chosenSubject of chosenSubjects">{{ chosenSubject }}</li>
-      </ul>
+      <div class="mt-10 flex flex-col bg-white px-6 py-8 rounded-lg shadow-lg text-black">
+        <ul class="space-y-8">
+          <li v-for="chosenSubject of chosenSubjects">{{ chosenSubject }}</li>
+        </ul>
+      </div>
+      <!-- countdown -->
+      <div class="relative mt-8">
+        <div class="absolute w-full h-4 bg-yellow-500 rounded-lg"></div>
+        <div class="absolute h-4 bg-yellow-700 transition-all duration-1000 ease-linear rounded-lg"
+          :style="{ width: countDownWidth }"></div>
+      </div>
     </template>
 
     <template v-if="gameState === GameState.GameOver">
-      Game over!
-      <form @submit.prevent="onConfirmScore" class="space-x-2">
-
-        <template v-for="chosenSubject of chosenSubjects">
-          <input v-model="guessedSubjects" type="checkbox" :value="chosenSubject" :name="chosenSubject"
-            :id="chosenSubject">
-          <label :for="chosenSubject"> {{ chosenSubject }}
+      <h2 class="font-semibold text-4xl my-8 text-center">Game over!</h2>
+      <form @submit.prevent="onConfirmScore"
+        class="flex flex-col bg-white px-6 py-8 rounded-lg shadow-lg text-black space-y-4">
+        <div v-for="chosenSubject of chosenSubjects">
+          <input class="ml-2 mr-4" v-model="guessedSubjects" type="checkbox" :value="chosenSubject"
+            :name="chosenSubject" :id="chosenSubject">
+          <label :for="chosenSubject" class="tracking-wide"> {{ chosenSubject }}
 
           </label>
-        </template>
-        <button type="submit" class="bg-blue-400 rounded py-2 px-4 my-2 text-white">Submit</button>
+        </div>
+        <button type="submit" class="bg-yellow-800 rounded py-2 px-4 my-2 text-white font-semibold">Submit</button>
       </form>
     </template>
     <template v-if="gameState === GameState.Victory">
-      {{ winningTeam?.name }} won!
-      <button @click="onPlayAgain" class="bg-blue-400 rounded py-2 px-4 my-2 text-white">Play again</button>
+      <div class="flex flex-col space-y-6 mt-12">
+        <h2 class="text-4xl text-center font-semibold">{{ winningTeam?.name }} won!</h2>
+        <h2 class="text-2xl text-center font-semibold">Final score</h2>
+        <div class="grid grid-cols-2 text-center">
+          <template v-for="team of sortedTeams">
+            <div>{{ team.name }}</div>
+            <div>{{ team.score }}</div>
+          </template>
+        </div>
+        <button @click="onPlayAgain" class="bg-yellow-800 rounded py-2 px-4 my-2 text-white">Play again</button>
+      </div>
     </template>
   </div>
 </template>
